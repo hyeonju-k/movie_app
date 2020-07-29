3d28e5056dc32bfdb7d219827d25fde8eeff43a4
HyeonJu <hj950000@ajou.ac.kr>
2020-07-29 17:32:41 +0900

    Updates

---
+++ precache-manifest.11d71a43eab7b78638abca18f5fce1f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7af7c0b9452b313735297a88fefb6a70",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "a2c5bb0246db42cd09e5",
+    "url": "/movie_app/static/css/main.ab70b619.chunk.css"
+  },
+  {
+    "revision": "a0054564cd7082ef0d3c",
+    "url": "/movie_app/static/js/2.59df1732.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app/static/js/2.59df1732.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2c5bb0246db42cd09e5",
+    "url": "/movie_app/static/js/main.8396cae8.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
